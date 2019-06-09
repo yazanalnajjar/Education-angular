c0ad0beb010794a7639fe442d0fb70114c4eaf5d
Yazan <yazannjjar@gmail.com>
2019-06-09 23:42:36 +0300

    Login

--- appNew1/src/app/app.module.ts
+++ appNew1/src/app/app.module.ts
@@ -4,26 +4,24 @@ import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
 
-    LoginComponent,
+    ,
 
-    HomeComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      {
-        path : 'login',
-        component : LoginComponent
-      },
+
+
+
+
     ])
   ],
   providers: [],
