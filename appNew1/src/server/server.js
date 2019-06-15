493315439b38f6e6d8e2700757e8d026f4636317
Yazan <yazannjjar@gmail.com>
2019-06-15 17:57:06 +0300

    1a

--- appNew1/src/server/server.js
+++ appNew1/src/server/server.js
@@ -73,67 +73,67 @@ app.post('/signupstudent' , function(req , res) {
 });
 
 
-//For Teacher
-app.post('/signupteacher' , function(req , res) {
-  // console.log(res);
-  const info = (req.body);
+//Sign in for student
 
+app.post('/signinstudent' , function(req, res){
+  const username = req.body.username;
 
-  // let firstname  = req.body.firstname;
-  // let lastname  = req.body.lastname;
-  let username = req.body.username;
-  let password = req.body.password;
-  let phonenumber = req.body.phonenumber;
-  let location  = req.body.location;
-  let email = req.body.email;
-  password = bcrypt.hashSync(info.password, 10);
+  const password = req.body.password;
 
-  student
-  .create({
-    // firstname : firstname,
-    // lastname : lastname,
-    username : username,
-    password : password,
-    phonenumber : phonenumber,
-    location : location,
-    email : email
-  })
-  .then(function() {
-    return res.status(201).send({ success: 'Sign up as engineer successful' });
-  })
-  .catch(function(err) {
-    if (err.name === 'SequelizeUniqueConstraintError') {
-      return res.status(400).send({ error: 'This username is already taken' });
+  //Check if users exists in the database
+  student.findOne({where : {username : username}}).then(function(user){
+    if(!user){
+      return res.status(401).send({error : 'Please sign up'});
     }
-    return res.status(500).send({ error: 'Server Error' });
-  });
-
-});
-
-  //Sign in for student
-
-  app.post('/signinstudent' , function(req, res){
-    const username = req.body.username;
-
-    const password = req.body.password;
-
-    //Check if users exists in the database
-    student.findOne({where : {username : username}}).then(function(user){
-      if(!user){
-        return res.status(401).send({error : 'Please sign up'});
-      }
-      //compare with stored password
-      const existingHashedPassword = user.password;
-      bcrypt.compare(password , existingHashedPassword).then(function(isMatching){
-        if(isMatching){
-          //create a token and send to client
-          const token = jwt.sign({username : user.username} , SECRET_KEY, {expiresIn : 4000});
-          return res.send({token : token});
-        }else {
-          return res.status(401).send({ error: 'Wrong password' });        }
-      })
+    //compare with stored password
+    const existingHashedPassword = user.password;
+    bcrypt.compare(password , existingHashedPassword).then(function(isMatching){
+      if(isMatching){
+        //create a token and send to client
+        const token = jwt.sign({username : user.username} , SECRET_KEY, {expiresIn : 4000});
+        return res.send({token : token});
+      }else {
+        return res.status(401).send({ error: 'Wrong password' });        }
     })
   })
+})
+//For Teacher
+// app.post('/signupteacher' , function(req , res) {
+//   // console.log(res);
+//   const info = (req.body);
+
+
+//   // let firstname  = req.body.firstname;
+//   // let lastname  = req.body.lastname;
+//   let username = req.body.username;
+//   let password = req.body.password;
+//   let phonenumber = req.body.phonenumber;
+//   let location  = req.body.location;
+//   let email = req.body.email;
+//   password = bcrypt.hashSync(info.password, 10);
+
+//   teacher
+//   .create({
+//     // firstname : firstname,
+//     // lastname : lastname,
+//     username : username,
+//     password : password,
+//     phonenumber : phonenumber,
+//     location : location,
+//     email : email
+//   })
+//   .then(function() {
+//     return res.status(201).send({ success: 'Sign up as engineer successful' });
+//   })
+//   .catch(function(err) {
+//     if (err.name === 'SequelizeUniqueConstraintError') {
+//       return res.status(400).send({ error: 'This username is already taken' });
+//     }
+//     return res.status(500).send({ error: 'Server Error' });
+//   });
+
+// });
+
 
 
   //Sign Up For teacher
@@ -202,74 +202,71 @@ app.post('/signupteacher' , function(req , res) {
       console.log(username);
       console.log(password);
 
-    student.findOne({where : {username : username}}).then(function(user){
-
-      if(!user){
-        return res.status(401).send({error : 'Please sign up'});
-      }
-      //compare with stored password
-      const existingHashedPassword = user.password;
-      bcrypt.compare(password , existingHashedPassword).then(function(isMatching){
-        if(isMatching){
-          //create a token and send to client
-          // res.send("It's working");
-          const token = jwt.sign({username : user.username} , SECRET_KEY, {expiresIn : 4000});
-          return res.send({token : token});
-        }else {
-          return res.status(401).send({ error: 'Wrong password' });
-            alert("Wrong Password");
+      teacher.findOne({where: {username: username}}).then(function(user){
+        if(!user){
+            return res.status(401).send({error: 'Please sign up'});
         }
-      })
-    })
+        //Compare with stored password
+        const existingHashedPassword = user.password;
+        bcrypt.compare(password, existingHashedPassword).then(function(isMatching){
+            if(isMatching){
+                //Create a token and send to client
+                const token = jwt.sign({username: user.username}, SECRET_KEY, {expiresIn: 4000});
+                return res.send({token: token});
+            } else {
+                return res.status(401).send({error: 'Wrong password'});
+            }
+        });
+    });
   })
 
-    const authenticate = function(req, res, next) {
-      const token = req.headers['x-access-token']; //Username encoded in token
-      if (!token) {
-        return res.status(401).send('Please sign in');
-      }
-      jwt.verify(token, SECRET_KEY, (err, data) => {
-        //console.log(data)
-        if (err) {
-          return res.status(401).send('Please sign in');
-        }
-        //Check if user exists in the database
-        const username = data.username;
+    // const authenticate = function(req, res, next) {
+    //   const token = req.headers['x-access-token']; //Username encoded in token
+    //   if (!token) {
+    //     return res.status(401).send('Please sign in');
+    //   }
+    //   jwt.verify(token, SECRET_KEY, (err, data) => {
+    //     //console.log(data)
+    //     if (err) {
+    //       return res.status(401).send('Please sign in');
+    //     }
+    //     //Check if user exists in the database
+    //     const username = data.username;
 
-        if (data.role) {
-          //console.log(username)
-          student
-            .findOne({ where: { username: username } })
-            .then((user) => {
-              //console.log(user)
-              if (!user) {
-                return res.status(401).send('Please sign up');
-              }
-              req.body.user = user; // put user in req.body
-              //console.log(user)
-              return next();
-            })
-            .catch(function(err) {
-              return res.status(500).send(err);
-            });
-        } else {
-          teacher
-            .findOne({ where: { username: username } })
-            .then((user) => {
-              //console.log(user)
-              if (!user) {
-                return res.status(401).send('Please sign up');
-              }
-              req.body.user = user; // put user in req.body
-              //console.log(user)
-              return next();
-            })
-            .catch(function(err) {
-              return res.status(500).send(err);
-            });
-        }
-      });
-    };
+    //     if (data.role) {
+    //       //console.log(username)
+    //       student
+    //         .findOne({ where: { username: username } })
+    //         .then((user) => {
+    //           //console.log(user)
+    //           if (!user) {
+    //             return res.status(401).send('Please sign up');
+    //           }
+    //           req.body.user = user; // put user in req.body
+    //           //console.log(user)
+    //           return next();
+    //         })
+    //         .catch(function(err) {
+    //           return res.status(500).send(err);
+    //         });
+    //     } else {
+    //       teacher
+    //         .findOne({ where: { username: username } })
+    //         .then((user) => {
+    //           //console.log(user)
+    //           if (!user) {
+    //             return res.status(401).send('Please sign up');
+    //           }
+    //           req.body.user = user; // put user in req.body
+    //           //console.log(user)
+    //           return next();
+    //         })
+    //         .catch(function(err) {
+    //           return res.status(500).send(err);
+    //         });
+    //     }
+    //   });
+    // };
 
   app.post('/teacherCourse' , function(req , res){
 
